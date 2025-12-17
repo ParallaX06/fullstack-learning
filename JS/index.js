59aa8934b338d5c438d5f1da0e7f9308190bde4e
Sarthik <sharmasarthik2006@gmail.com>
2025-12-17 22:15:28 +0530

    tried some javascript syntax

---
+++ JS/index.js
@@ -0,0 +1,17 @@
+//let myAge = 19
+//console.log(myAge)
+
+/*let humanAge = 19
+let humanDogRatio = 7
+let dogAge = humanAge * humanDogRatio
+
+console.log(dogAge)*/
+
+
+let bP = 50
+
+bP = bP + 100
+bP = bP - 25
+bP = bP + 70
+
+console.log(bP)
