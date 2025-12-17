3f21ff49c77941d376f608733913195a47bf085e
Sarthik <sharmasarthik2006@gmail.com>
2025-12-17 23:36:20 +0530

    wrote javascript code function to increment number on click

--- JS/projects/counter-app/index.js
+++ JS/projects/counter-app/index.js
@@ -1,3 +1,14 @@
-function incremetn() {
-    print("The button was clicked")
+//initialize a count to store clicks
+//listen for clicks when the button is pressed
+//increment the count variable by 1 each time the button is clicked
+//change count-el to reflect increment in html page
+
+let countEl = document.getElementById("count-el")
+
+let count = 0
+
+function increment() {
+    count = count + 1
+    countEl.innerText = count
 }
+
