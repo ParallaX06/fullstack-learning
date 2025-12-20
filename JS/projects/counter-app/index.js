42812e463e57bf1a5c2abbd8066efeefbbb7fd31
Sarthik <sharmasarthik2006@gmail.com>
2025-12-20 12:19:05 +0530

    added Save button which reflects the current counter number

--- JS/projects/counter-app/index.js
+++ JS/projects/counter-app/index.js
@@ -1,14 +1,13 @@
-//initialize a count to store clicks
-//listen for clicks when the button is pressed
-//increment the count variable by 1 each time the button is clicked
-//change count-el to reflect increment in html page
 
 let countEl = document.getElementById("count-el")
 
 let count = 0
 
 function increment() {
-    count = count + 1
+    count += 1
     countEl.innerText = count
 }
 
+function save() {
+    console.log(count)
+}
