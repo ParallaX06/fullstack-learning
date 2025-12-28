81ed451eacdf9236df5feaa8c2eca469aaa08877
Sarthik <sharmasarthik2006@gmail.com>
2025-12-28 16:21:29 +0530

    back on track

---
+++ JS/challenge/index.js
@@ -0,0 +1,6 @@
+let error = document.getElementById("error")
+
+function purchase() {
+    console.log("button clicked")
+    error.textContent = "Something went wrong, please try again later!"
+}
---
+++ JS/function/script1.js
@@ -0,0 +1,8 @@
+//document.getElementById("myh1").innerText = 'hello world';
+
+let name;
+let greeting;
+name = 'Sarthik';
+greeting = 'Hi, my name is';
+
+console.log(greeting + " " + name);
